53de415b8dd78c64345dab6e8b7793c8fb335d1e
bruno larocca <brunolujanlarocca@gmail.com>
2023-06-22 11:07:25 -0300

    First Class - Position

--- src/app/Program.java
+++ src/app/Program.java
@@ -1,7 +1,11 @@
 package app;
 
+import boardgame.Position;
+
 public class Program {
     public static void main(String[] args) {
-        System.out.println("hello world!");
+
+        Position pos = new Position(3,5);
+        System.out.println(pos);
     }
 }
